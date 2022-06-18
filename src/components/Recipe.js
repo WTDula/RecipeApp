07ef8775e34b4466563d083c77b72ee18749c3cd
WTDula <wtdula@gmail.com>
2022-06-18 13:05:56 -0500

    some layout/color changes to index.css, variables in Recipe.js changed to dot notation

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Recipe = ({recipe}) => {
     return  (
         <div className="recipe">
-            <img className="recipe-img" src={recipe["recipe"]["image"]} alt=""/>
-            <p className="recipe-name">{recipe["recipe"]["label"]}</p>
+            <img className="recipe-img" src={recipe.recipe.image} alt=""/>
+            <p className="recipe-name">{recipe.recipe.label}</p>
         </div>
         )
 }
