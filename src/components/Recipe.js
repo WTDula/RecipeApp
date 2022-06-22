e8eb65e4679734bc7eef5d8197ee0638f7064c3e
WTDula <wtdula@gmail.com>
2022-06-21 23:58:07 -0500

    restructuring Recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Recipe = ({recipe}) => {
     return  (
-        <div>
-            <div className="recipe">
-                <img className="recipe-img" src={recipe.recipe.image} alt=""/>
-                <p className="recipe-name">{recipe.recipe.label}</p>
-            </div>
+        <div className="recipe">
+            <img className="recipe-img" src={recipe.recipe.image} alt=""/>
+            <p className="recipe-name">{recipe.recipe.label}</p>
             <div className='recipe-ingredients'>
                 <h2>Ingredients</h2>
                 {recipe.recipe.ingredients}
