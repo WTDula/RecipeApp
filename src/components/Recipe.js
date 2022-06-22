4317accae5f1a5512aca7fb4451d7322abc0a7cf
WTDula <wtdula@gmail.com>
2022-06-21 23:56:11 -0500

    adding ingredient list to each recipe to be in overlay when hovering over by cursor

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Recipe = ({recipe}) => {
     return  (
-        <div className="recipe">
-            <img className="recipe-img" src={recipe.recipe.image} alt=""/>
-            <p className="recipe-name">{recipe.recipe.label}</p>
+        <div>
+            <div className="recipe">
+                <img className="recipe-img" src={recipe.recipe.image} alt=""/>
+                <p className="recipe-name">{recipe.recipe.label}</p>
+            </div>
+            <div className='recipe-ingredients'>
+                <h2>Ingredients</h2>
+                {recipe.recipe.ingredients}
+            </div>
         </div>
         )
 }
